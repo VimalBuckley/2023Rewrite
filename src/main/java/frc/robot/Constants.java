92c4324eecd2a8ed86ac6166739cf4e50a252a1b
TruestGecko <92557966+VimalBuckley@users.noreply.github.com>
2023-10-28 19:48:56 -0500

    Updated Swerve Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,17 +11,19 @@ public class Constants {
 	}
 
 	public static class SwerveConstants {
-		
-		public static final double MAX_LINEAR_SPEED = 0;
-		public static final double MAX_LINEAR_ACCELERATION = 0; 
-		public static final double MAX_ROTATIONAL_SPEED = 0.0;
-		public static final double MAX_ROTATIONAL_ACCELERATION = 0.0;
-
-		public static final double DRIVE_RATIO = 1 / 1.; // drive rotations per motor rotation
-		public static final double ANGLE_RATIO = 1 / 1.; // angle rotations per motor rotation
+		public static final double MAX_LINEAR_SPEED =
+			((1276 * 9.42) / 60) / 12 * 0.3048; // 1276 is rpm, 9.42 is wheel circumference (in.), final units are m/s
+		public static final double MAX_LINEAR_ACCELERATION = 4; // Random number
+		public static final double MAX_ROTATIONAL_SPEED =
+			MAX_LINEAR_SPEED / (4 / 3); // 4/3 is (about) the radius from the center of the robot to the swerve drive wheels.
+		public static final double MAX_ROTATIONAL_ACCELERATION = 4; // Linear Acceleration/radius
+		/** Drive rotations per motor rotation */
+		public static final double DRIVE_RATIO = 1 / 5.;
+		/** Angle rotations per motor rotation */
+		public static final double ANGLE_RATIO = 1 / 6.75;
 
 		public static final SwerveMotorConfig FRONT_LEFT_DRIVE_CONFIG = new SwerveMotorConfig(
-			0, 
+			3, 
 			true, 
 			true, 
 			false, 
@@ -29,19 +31,19 @@ public class Constants {
 			new PIDConstants(0.075, 0, 0)
 		);
 		public static final SwerveMotorConfig FRONT_LEFT_ANGLE_CONFIG = new SwerveMotorConfig(
-			0, 
+			7, 
 			false, 
 			false, 
 			false, 25, 
 			new PIDConstants(0.3, 0, 0)
 		);
 		public static final Translation2d FRONT_LEFT_MODULE_TRANSLATION = new Translation2d(
-			0,
-			0
+			0.3175,
+			0.2413
 		);
 
 		public static final SwerveMotorConfig FRONT_RIGHT_DRIVE_CONFIG = new SwerveMotorConfig(
-			0, 
+			4, 
 			true, 
 			false, 
 			false, 
@@ -49,7 +51,7 @@ public class Constants {
 			new PIDConstants(0.05, 0, 0)
 		);
 		public static final SwerveMotorConfig FRONT_RIGHT_ANGLE_CONFIG = new SwerveMotorConfig(
-			0, 
+			8, 
 			false, 
 			false, 
 			false, 
@@ -57,12 +59,12 @@ public class Constants {
 			new PIDConstants(0.3, 0, 0)
 		);
 		public static final Translation2d FRONT_RIGHT_MODULE_TRANSLATION = new Translation2d(
-			0,
-			0
+			0.3175,
+			-0.2413
 		);
 
 		public static final SwerveMotorConfig BACK_LEFT_DRIVE_CONFIG = new SwerveMotorConfig(
-			0, 
+			2, 
 			true, 
 			true, 
 			false, 
@@ -70,19 +72,19 @@ public class Constants {
 			new PIDConstants(0.075, 0, 0)
 		);
 		public static final SwerveMotorConfig BACK_LEFT_ANGLE_CONFIG = new SwerveMotorConfig(
-			0, 
+			6, 
 			false, 
 			false, 
 			false, 25, 
 			new PIDConstants(0.25, 0, 0)
 		);
 		public static final Translation2d BACK_LEFT_MODULE_TRANSLATION = new Translation2d(
-			0,
-			0
+			-0.3175,
+			0.2413
 		);
 
 		public static final SwerveMotorConfig BACK_RIGHT_DRIVE_CONFIG = new SwerveMotorConfig(
-			0, 
+			9, 
 			true, 
 			false, 
 			false, 
@@ -90,7 +92,7 @@ public class Constants {
 			new PIDConstants(0.05, 0, 0)
 		);
 		public static final SwerveMotorConfig BACK_RIGHT_ANGLE_CONFIG = new SwerveMotorConfig(
-			0, 
+			5, 
 			false, 
 			false, 
 			false, 
@@ -98,11 +100,11 @@ public class Constants {
 			new PIDConstants(0.3, 0, 0)
 		);
 		public static final Translation2d BACK_RIGHT_MODULE_TRANSLATION = new Translation2d(
-			0, 
-			0
+			-0.3175,
+			-0.2413
 		);
 		
-		public static final double WHEEL_DIAMETER = 0.0;
+		public static final double WHEEL_DIAMETER = 0.0762; // in meters
 	}
 
 	public static class VisionConstants {
