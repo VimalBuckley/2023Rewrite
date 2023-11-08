6fba9bedd4b87d09ce5904c1b0ec44430e0477d6
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-11-08 10:40:45 -0600

    Reintroduce Custom Module Optimization
    I am sorry for file length, Vimal

--- src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
@@ -43,7 +43,7 @@ public class SwerveModule {
 	}
 
 	public void drive(SwerveModuleState initialTargetState) {
-		SwerveModuleState targetState = SwerveModuleState.optimize(
+		SwerveModuleState targetState = optimizeTalon(
 			initialTargetState,
 			getModuleState().angle
 		);
@@ -93,6 +93,79 @@ public class SwerveModule {
 			2 /
 			(SwerveConstants.DRIVE_RATIO * SwerveConstants.WHEEL_DIAMETER)
 		);
+	}
+
+	/**
+	 * Minimize the change in heading the desired swerve module state would require
+	 * by potentially
+	 * reversing the direction the wheel spins. Customized from WPILib's version to
+	 * include placing
+	 * in appropriate scope for CTRE onboard control.
+	 * <p>
+	 * The WPI optimize function assumes that the pid controller is continuous,
+	 * which
+	 * means that if you are at 359 degrees and set a target of 1 degree, it assumes
+	 * the controller will be able to tell the module to move 2 degrees forwards and
+	 * not 358 degrees backwards (which is how the wpilib pid controller functions).
+	 * <p>
+	 * However CTRE onboard pid controllers are not continuous, if it’s at a
+	 * setpoint at 359 and you tell it to go 1, it will rotate 358 degrees backwards
+	 * instead of 2 degrees forwards. If you want it to go to 1 you actually need to
+	 * tell it to go to 361 degrees. To do this you need the custom optimize
+	 * function defined here.
+	 * <p>
+	 * 
+	 * @see <a
+	 *      href=https://www.chiefdelphi.com/t/swerve-modules-flip-180-degrees-periodically-conditionally/393059/3
+	 *      >Chief Delphi Post Concerning The Issue</a>
+	 * 
+	 * @param desiredState The desired state.
+	 * @param currentAngle The current module angle.
+	 */
+	public static SwerveModuleState optimizeTalon(SwerveModuleState desiredState, Rotation2d currentAngle) {
+		double targetAngle = placeInAppropriate0To360Scope(currentAngle.getDegrees(), desiredState.angle.getDegrees());
+		double targetSpeed = desiredState.speedMetersPerSecond;
+		double delta = targetAngle - currentAngle.getDegrees();
+		if (Math.abs(delta) > 90) {
+			targetSpeed = -targetSpeed;
+			targetAngle = delta > 90 ? (targetAngle -= 180) : (targetAngle += 180);
+		}
+		return new SwerveModuleState(
+				targetSpeed,
+				Rotation2d.fromDegrees(targetAngle));
+	}
+
+	/**
+	 * Places the given angle in the appropriate 0 to 360 degree scope based on the
+	 * reference angle.
+	 * 
+	 * @param scopeReference the reference angle to base the scope on
+	 * @param newAngle       the angle to place in the scope
+	 * @return the new angle within the appropriate 0 to 360 degree scope
+	 */
+	private static double placeInAppropriate0To360Scope(double scopeReference, double newAngle) {
+		double lowerBound;
+		double upperBound;
+		double lowerOffset = scopeReference % 360;
+		if (lowerOffset >= 0) {
+			lowerBound = scopeReference - lowerOffset;
+			upperBound = scopeReference + (360 - lowerOffset);
+		} else {
+			upperBound = scopeReference - lowerOffset;
+			lowerBound = scopeReference - (360 + lowerOffset);
+		}
+		while (newAngle < lowerBound) {
+			newAngle += 360;
+		}
+		while (newAngle > upperBound) {
+			newAngle -= 360;
+		}
+		if (newAngle - scopeReference > 180) {
+			newAngle -= 360;
+		} else if (newAngle - scopeReference < -180) {
+			newAngle += 360;
+		}
+		return newAngle;
 	}
 
 	public static class SwerveMotorConfig {
